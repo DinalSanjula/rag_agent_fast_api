bcff9cc9b73aa3815b14b017f9e4977c4bbb218e
DinalSanjula <dinalsanjulanw@gmail.com>
2025-12-31 15:53:25 +0530

    enhanced query

--- rag_agent_fast_api.py
+++ rag_agent_fast_api.py
@@ -38,25 +38,56 @@ vector_store = PineconeVectorStore(index_name="weekdayhrdocument",embedding=embe
 
 # vector_store.add_documents(documents=text_splits)#stores the vector embeddings to retrive later
 
-retrieve_documents = vector_store.similarity_search("what is the promotion policy")
 
 llm_model = ChatOllama(model="llama3.2:latest")
 
 @tool
-def retrieve_context(query:str):
-    """Retrieve information from vector database to help answer user queries"""
-    retrieved_documents = vector_store.similarity_search(query,k=5)
+def retrieve_context(enhanced_user_query:str):
+    """Retrieve information to hel[ answer user queries using enhanced_query
+    returend from the enhance_user_query tool"""
+    retrieved_documents = vector_store.similarity_search(enhanced_user_query,k=5)
 
     content = "\n\n".join(
         (f"Source: {doc.metadata} \nContent : {doc.page_content}" for doc in retrieved_documents)
     )
     return content
+
+@tool
+def enhance_user_query(user_query: str):
+    """
+    Enhance user query using an LLM for better retrieval
+
+    Args:
+        user_query: The user's query
+
+    Returns:
+        Enhanced query optimized for semantic search
+    """
+
+    enhancement_prompt = ChatPromptTemplate.from_template(
+        """
+        You are a query optimization expert for an HR knowledge base.
+        Your task is to expand users query to improve document retrieval
+        
+        Original query: {original_query}
+        
+        Instructions:
+        - Expand abbreviation ( PTO - paid time off, HR - human resource, etc)
+        - Include releven keywords for HR context
+        - Maintain original intent
+        """
+    )
+
+    enhancement_chain = enhancement_prompt | llm_model
+    resp = enhancement_chain.invoke({"original_query" : user_query})
+    return resp.content
+
 system_prompt = """
-You have access to a tool that retrieves context from hr documents.
+You have access to a tools that enhances user  wuery and retrieves context from hr documents.
 Use the tool to help answer user queries.
 """
 
-tools = [retrieve_context]
+tools = [retrieve_context,enhance_user_query]
 agent = create_agent(model=llm_model,system_prompt=system_prompt,tools=tools)
 
 # while 1:
